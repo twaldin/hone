3068e14b2f4707475d408acf5a836206354e9254
twaldin <timothy@waldin.net>
2026-07-17 10:25:50 -0700

    Allow complete mutation tool calls

--- optimizer/worker/mutate.ts
+++ optimizer/worker/mutate.ts
@@ -166,7 +166,7 @@ function buildRegistry(env: SessionEnv, authStorage: AuthStorage): ModelRegistry
         supportsTools: true,
         cost: { input: 0, output: 0, cacheRead: 0, cacheWrite: 0 },
         contextWindow: 200_000,
-        maxTokens: 16_384,
+        maxTokens: 32_768,
       },
     ],
   });
